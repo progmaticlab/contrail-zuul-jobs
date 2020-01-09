8d92c3fe85e358a828a5369ce8447917aeeb66b5
Andrey Pavlov <andrey.mp@gmail.com>
2020-01-09 11:12:15 +0300

    migrate contrail-analytics to tf-analytics

--- roles/contrail-unittests-tntestr/files/contrail-unittests-gather.rb
+++ roles/contrail-unittests-tntestr/files/contrail-unittests-gather.rb
@@ -17,7 +17,7 @@ project = "tools/sandesh" if ENV["ZUUL_PROJECT"] =~ /contrail-sandesh/
 project = "tools/generateds" if ENV["ZUUL_PROJECT"] =~ /contrail-generateDS/
 project = "vrouter" if ENV["ZUUL_PROJECT"] =~ /contrail-vrouter/
 project = "src/contrail-common" if ENV["ZUUL_PROJECT"] =~ /contrail-common/
-project = "src/contrail-analytics" if ENV["ZUUL_PROJECT"] =~ /contrail-analytics/
+project = "src/contrail-analytics" if ENV["ZUUL_PROJECT"] =~ /tf-analytics/
 project = "src/contrail-api-client" if ENV["ZUUL_PROJECT"] =~ /contrail-api-client/
 
 STDERR.puts "contrail-unittest-gather.rb: Review for project #{ENV["ZUUL_PROJECT"]}\n"
